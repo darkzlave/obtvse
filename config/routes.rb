bbe7d1938942b096da25a0c506cf1636815fcb8f
Phillipe Casorla Sagot <phillipe@pota-toss.com>
2012-12-03 21:12:37 -0600

    Added route for kudos post

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Obtvse::Application.routes.draw do
   match '/admin', :to => 'posts#admin'
   match '/new', :to => 'posts#new'
   match '/edit/:id', :to => 'posts#edit'
+  post '/:slug/kudos', :to => 'posts#kudos', :as => 'post'
   post '/preview', :to => 'posts#preview'
   put '/preview', :to => 'posts#preview'
+  match "mu-0ee6831b-17e743aa-f068c19f-5f1e3a36" => "blitz#d42"
   get '/:slug', :to => 'posts#show', :as => 'post'
   delete '/:slug', :to => 'posts#destroy', :as  => 'post'
   put '/:slug', :to => 'posts#update', :as  => 'post'
+
   root :to => 'posts#index'
 end
